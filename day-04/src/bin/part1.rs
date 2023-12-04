c61795e6cee90f80d6cbb6c9949843097830abe3
talves <tony@3alves.com>
2023-12-04 08:20:23 -0800

    day-04: tests update

--- day-04/src/bin/part1.rs
+++ day-04/src/bin/part1.rs
@@ -17,7 +17,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn part1_works() {
         let result = part1(
             "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
