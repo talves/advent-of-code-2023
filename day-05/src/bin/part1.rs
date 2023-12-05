e5c8359fd65a8ee25a12e31dea360daf28f82e5f
talves <tony@3alves.com>
2023-12-05 08:59:59 -0800

    day-04: part1

---
+++ day-05/src/bin/part1.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> i32 {
+    35
+}
+
+fn part1(input: &str) -> i32 {
+    process(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        let result = part1(
+            "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4",
+        );
+        assert_eq!(result, 35);
+    }
+}
