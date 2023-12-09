e929ec2c45fc6ef055b2c5b5c90586d674cd1a4f
talves <tony@3alves.com>
2023-12-09 10:40:17 -0800

    day-08: part2 working tests

--- day-08/src/bin/part2.rs
+++ day-08/src/bin/part2.rs
@@ -1,14 +1,83 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("../input.txt");
     let output = part2(input);
     dbg!(output);
 }
 
-fn process(input: &str) -> String {
-    format!("Hello, {}", input)
+enum Direction {
+    Left,
+    Right,
 }
 
-fn part2(input: &str) -> String {
+fn parse_input(input: &str) -> (Vec<Direction>, HashMap<&str, (&str, &str)>, Vec<&str>) {
+    let mut lines = input.lines().into_iter();
+    let mut start_keys = vec![];
+    let directions: Vec<Direction> = lines
+        .next()
+        .unwrap()
+        .chars()
+        .map(|d| match d {
+            'R' => Direction::Right,
+            'L' => Direction::Left,
+            _ => panic!("Should not be the case, bad directions line"),
+        })
+        .collect::<Vec<Direction>>();
+    let mut mapping: HashMap<&str, (&str, &str)> = HashMap::new();
+    for line in lines {
+        if !line.is_empty() {
+            // example: FKX = (LSH, TSV)
+            let key = &line[0..3];
+            if &key[2..3] == "A" {
+                start_keys.push(key);
+            }
+            let left = &line[7..10];
+            let right = &line[12..15];
+            mapping.insert(key, (left, right));
+        }
+    }
+    (directions, mapping, start_keys)
+}
+
+fn process(input: &str) -> u32 {
+    let (directions, paths, start_keys) = parse_input(input);
+    // dbg!(&start_keys);
+    // dbg!(&paths);
+    let mut found_z_end = false;
+    let mut current_keys: Vec<&str> = start_keys.iter().map(|x| *x).collect::<Vec<&str>>();
+    let mut count = 0;
+    while !found_z_end {
+        for direction in &directions {
+            let mut new_keys: Vec<&str> = Vec::new();
+            current_keys
+                .iter()
+                .for_each(|current_key| match paths.get(*current_key) {
+                    Some((left, right)) => match direction {
+                        Direction::Left => new_keys.push(left),
+                        Direction::Right => new_keys.push(right),
+                    },
+                    None => panic!("Paths found unmapped key {}", current_key),
+                });
+            count += 1;
+            found_z_end = true;
+            current_keys = Vec::new();
+            for current_key in new_keys {
+                current_keys.push(current_key);
+                if &current_key[2..3] != "Z" {
+                    found_z_end = false;
+                }
+            }
+            if found_z_end {
+                break;
+            }
+            // dbg!(&current_keys);
+        }
+    }
+    count
+}
+
+fn part2(input: &str) -> u32 {
     process(input)
 }
 
@@ -18,7 +87,19 @@ mod tests {
 
     #[test]
     fn part2_works() {
-        let result = part2("Advent of Code!");
-        assert_eq!(result, "Hello, Advent of Code!");
+        let result = part2(
+            "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+",
+        );
+        assert_eq!(result, 6);
     }
 }
