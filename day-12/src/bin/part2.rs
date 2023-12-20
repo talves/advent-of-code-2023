30bf4fcfd48c07dc23752c927915dd7bc6488f4f
Tony Alves <tony@3alves.com>
2023-12-19 21:39:36 -0800

    day-12: part2 tests

--- day-12/src/bin/part2.rs
+++ day-12/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{str::FromStr, time::Instant};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -8,7 +8,9 @@ fn main() {
 
 fn process(input: &str) -> u64 {
     let report = Report::from_str(input).unwrap();
-    report.lines.iter().map(|line| line.get_count()).sum()
+    // report.iterate_sum(5)
+    // report.sum()
+    report.sum_build(5)
 }
 
 fn part2(input: &str) -> u64 {
@@ -62,6 +64,70 @@ impl Report {
     fn new() -> Report {
         Report { lines: Vec::new() }
     }
+    fn sum(&self) -> u64 {
+        let mut line_count = 0;
+        self.lines
+            .iter()
+            .map(|line| {
+                line_count += 1;
+                println!("Line {:?}: {:?}", line_count, &line.original);
+                let start = Instant::now();
+                let sum_count = line.get_count();
+                let duration = start.elapsed();
+                // dbg!(&sum_count);
+                println!("Count: {:?} Time elapsed: {:?}", sum_count, duration);
+                sum_count
+            })
+            .sum()
+    }
+    fn sum_build(&self, count: usize) -> u64 {
+        let mut line_count = 0;
+        self.lines
+            .iter()
+            .map(|line| {
+                // build the new input str for the line
+                let mut arrangement_str = line.arrangement[0].to_string();
+                (1..line.arrangement.len()).for_each(|x| {
+                    arrangement_str =
+                        [&arrangement_str, ",", &line.arrangement[x].to_string()].concat();
+                });
+                let mut input = "";
+                let binding = [input, &line.original, " "].concat().repeat(count);
+                let binding = binding.trim().replace(" ", "?");
+                input = &binding;
+                let binding = [&arrangement_str, " "].concat().repeat(count);
+                let binding = binding.trim().replace(" ", ",");
+                let binding = [input, " ", &binding].concat();
+                input = &binding;
+                dbg!(&input);
+                let new_line = &Report::from_str(input).unwrap().lines[0];
+                line_count += 1;
+                println!("Line {:?}: {:?}", line_count, &line.original);
+                let start = Instant::now();
+                let sum_count = new_line.get_count();
+                let duration = start.elapsed();
+                // dbg!(&sum_count);
+                println!("Count: {:?} Time elapsed: {:?}", sum_count, duration);
+                sum_count
+            })
+            .sum()
+    }
+    fn iterate_sum(&self, count: usize) -> u64 {
+        let mut line_count = 0;
+        self.lines
+            .iter()
+            .map(|line| {
+                line_count += 1;
+                println!("Line {:?}: {:?}", line_count, &line.original);
+                let start = Instant::now();
+                let sum_count = line.iterate_sum(count);
+                let duration = start.elapsed();
+                // dbg!(&sum_count);
+                println!("Count: {:?} Time elapsed: {:?}", sum_count, duration);
+                sum_count
+            })
+            .sum()
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -110,7 +176,99 @@ where
 }
 
 impl ReportLine {
-    fn get_count(&self) -> u64 {
+    fn iterate_sum(&self, count: usize) -> u64 {
+        fn get_sum(
+            line: &ReportLine,
+            count: usize,
+            original: &String,
+            original_arrangement: &String,
+        ) -> Vec<u64> {
+            if count == 0 {
+                vec![0u64]
+            } else if count == 1 {
+                let pcount = line.get_count() as u64;
+                // println!("End of permutations {}", pcount);
+                vec![pcount]
+            } else {
+                let count = count - 1;
+                line.get_unique_permutations()
+                    .iter()
+                    .map(|permutation| {
+                        // dbg!(&permutation);
+                        let mut str_permutation = line.original.to_string();
+                        permutation.iter().enumerate().for_each(|(idx, x)| {
+                            let ch = match *x {
+                                0u8 => "#",
+                                1u8 => ".",
+                                _ => "",
+                            };
+                            str_permutation
+                                .replace_range(line.unknown[idx]..=line.unknown[idx], ch);
+                            // we should not need this next line, but keeping it for debug
+                            // input = input.replace("?", ".");
+                        });
+
+                        // let mut input = [original, "?", &str_permutation].concat();
+                        let mut input = [&str_permutation, "?", original].concat();
+                        let mut arrangement_str = line.arrangement[0].to_string();
+                        (1..line.arrangement.len()).for_each(|x| {
+                            arrangement_str =
+                                [&arrangement_str, ",", &line.arrangement[x].to_string()].concat();
+                        });
+                        input = [
+                            input,
+                            " ".to_owned(),
+                            arrangement_str,
+                            ",".to_owned(),
+                            original_arrangement.to_string(),
+                        ]
+                        .concat();
+                        // dbg!(&input);
+                        let report = Report::from_str(&input).unwrap();
+                        report
+                            .lines
+                            .iter()
+                            .map(|line| {
+                                get_sum(line, count, original, original_arrangement)
+                                    .iter()
+                                    .sum::<u64>()
+                            })
+                            .sum::<u64>()
+                    })
+                    .collect::<Vec<u64>>()
+            }
+        }
+        let mut arrangement_str = self.arrangement[0].to_string();
+        (1..self.arrangement.len()).for_each(|x| {
+            arrangement_str = [&arrangement_str, ",", &self.arrangement[x].to_string()].concat();
+        });
+        // let line: ReportLine = ReportLine {
+        //     original: self.original.clone(),
+        //     sequence: self.sequence.clone(),
+        //     arrangement: self.arrangement.clone(),
+        //     unknown: self.unknown.clone(),
+        //     damaged: self.damaged.clone(),
+        // };
+        // let input = &[
+        //     self.original.clone(),
+        //     "?".to_owned(),
+        //     self.original.clone(),
+        //     " ".to_owned(),
+        //     arrangement_str.clone(),
+        //     ",".to_owned(),
+        //     arrangement_str.clone(),
+        // ]
+        // .concat();
+        // dbg!(&input);
+        let line = &Report::from_str(&[&self.original, " ", &arrangement_str.clone()].concat())
+            .unwrap()
+            .lines[0];
+        get_sum(&line, count, &self.original, &arrangement_str)
+            .iter()
+            .sum()
+    }
+
+    fn get_unique_permutations(&self) -> Vec<Vec<u8>> {
         let damaged_count = self.damaged.len();
         let unknown_count = self.unknown.len();
         let damaged_needed: usize = self.arrangement.iter().map(|x| x).sum::<usize>();
@@ -118,6 +276,7 @@ impl ReportLine {
         let skips_needed: usize = unknown_count - damaged_missing;
 
         // dbg!(&self.original);
+        // dbg!(format!("{:?}", &self.arrangement));
         // dbg!(&damaged_count);
         // dbg!(&unknown_count);
         // dbg!(&damaged_needed);
@@ -125,9 +284,11 @@ impl ReportLine {
         // dbg!(&skips_needed);
 
         // Create a mapping of our needed damaged fill using index of &str example: "##."
-        let mut char_combination: Vec<char> = Vec::new();
-        (0..damaged_missing).for_each(|_| char_combination.push('#'));
-        (0..skips_needed).for_each(|_| char_combination.push('.'));
+        // We are going to use u8 (1 byte) instead of the char for faster and less memory
+        // '#' == Damaged == 0, '.' == Operational == 1
+        let mut char_combination: Vec<u8> = Vec::new();
+        (0..damaged_missing).for_each(|_| char_combination.push(0u8));
+        (0..skips_needed).for_each(|_| char_combination.push(1u8));
 
         // The sequence we are looking for from original
         let sequence_needed: Vec<(Spring, usize)> = self
@@ -136,21 +297,25 @@ impl ReportLine {
             .map(|count| (Spring::Damaged, *count))
             .collect::<Vec<(Spring, usize)>>();
         // dbg!(format!("{:?}", &sequence_needed));
-        let mut dedups_list: Vec<String> = Vec::new();
+        let mut dedups_list: Vec<Vec<u8>> = Vec::new();
 
         // Get unique permutations of the chars needed above
-        let count: u64 = unique_permutations(char_combination)
+        unique_permutations(char_combination)
             .iter()
-            .map(|permutation| {
-                let mut workstr = self.original.to_string();
-                permutation.iter().enumerate().for_each(|(idx, ch)| {
-                    workstr.replace_range(self.unknown[idx]..=self.unknown[idx], &*ch.to_string());
-                    // we should not need this next line, but keeping it for debug
-                    // workstr = workstr.replace("?", ".");
-                });
-                if dedups_list.contains(&workstr) {
-                    0u64
-                } else {
+            .for_each(|permutation| {
+                if !dedups_list.contains(permutation) {
+                    let mut workstr = self.original.to_string();
+                    permutation.iter().enumerate().for_each(|(idx, x)| {
+                        let ch = match *x {
+                            0u8 => "#",
+                            1u8 => ".",
+                            _ => "",
+                        };
+                        workstr.replace_range(self.unknown[idx]..=self.unknown[idx], ch);
+                        // we should not need this next line, but keeping it for debug
+                        // workstr = workstr.replace("?", ".");
+                    });
+                    // dbg!(&workstr);
                     let sequence = Self::get_sequence_from(&workstr)
                         .iter()
                         .filter(|(spring, _count)| *spring == Spring::Damaged)
@@ -158,16 +323,18 @@ impl ReportLine {
                         .collect::<Vec<(Spring, usize)>>();
                     // dbg!(format!("{:?}", &sequence));
                     if sequence == sequence_needed {
-                        dedups_list.push(workstr);
-                        1u64
-                    } else {
-                        0u64
+                        dedups_list.push(permutation.clone());
                     }
                 }
-            })
-            .sum();
-        // dbg!(&count);
-        count
+            });
+        // dbg!(&dedups_list);
+        dedups_list
+    }
+    fn get_count(&self) -> u64 {
+        Self::get_unique_permutations(&self)
+            .len()
+            .try_into()
+            .unwrap()
     }
 
     fn get_sequence_from(line: &String) -> Vec<(Spring, usize)> {
@@ -287,17 +454,17 @@ impl ReportLine {
 mod tests {
     use super::*;
 
-    #[test]
-    fn part2_works() {
-        let input = "???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1";
-        let result = part2(input);
-        assert_eq!(result, 21);
-    }
+    //     #[test]
+    //     fn part2_works() {
+    //         let input = "???.### 1,1,3
+    // .??..??...?##. 1,1,3
+    // ?#?#?#?#?#?#?#? 1,3,1,6
+    // ????.#...#... 4,1,1
+    // ????.######..#####. 1,6,5
+    // ?###???????? 3,2,1";
+    //         let result = part2(input);
+    //         assert_eq!(result, 21);
+    //     }
 
     #[test]
     fn from_str_works() {
@@ -310,6 +477,7 @@ mod tests {
         let result = Report::from_str(input).unwrap();
         assert_eq!(result.lines.len(), 6);
         assert_eq!(result.lines[0].arrangement, vec![1, 1, 3]);
+        dbg!(&result.lines[0].original);
         assert_eq!(result.lines[1].arrangement, vec![1, 1, 3]);
         assert_eq!(result.lines[2].arrangement, vec![1, 3, 1, 6]);
         assert_eq!(result.lines[3].arrangement, vec![4, 1, 1]);
@@ -328,7 +496,14 @@ mod tests {
 ?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
-?###???????? 3,2,1";
+?###???????? 3,2,1
+";
+        //         let input = "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3
+        // .??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##. 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3
+        // ?#?#?#?#?#?#?#???#?#?#?#?#?#?#???#?#?#?#?#?#?#???#?#?#?#?#?#?#???#?#?#?#?#?#?#? 1,3,1,6,1,3,1,6,1,3,1,6,1,3,1,6,1,3,1,6
+        // ????.#...#...?????.#...#...?????.#...#...?????.#...#...?????.#...#... 4,1,1,4,1,1,4,1,1,4,1,1,4,1,1
+        // ????.######..#####.?????.######..#####.?????.######..#####.?????.######..#####.?????.######..#####. 1,6,5,1,6,5,1,6,5,1,6,5,1,6,5
+        // ?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1";
         let result = Report::from_str(input).unwrap();
         assert_eq!(result.lines.len(), 6);
         assert_eq!(result.lines[0].get_count(), 1);
@@ -337,5 +512,29 @@ mod tests {
         assert_eq!(result.lines[3].get_count(), 1);
         assert_eq!(result.lines[4].get_count(), 4);
         assert_eq!(result.lines[5].get_count(), 10);
+        // assert_eq!(result.lines[6].get_count(), 1);
+        // assert_eq!(result.lines[7].get_count(), 16384);
+        // assert_eq!(result.lines[8].get_count(), 1);
+        // assert_eq!(result.lines[9].get_count(), 16);
+        // assert_eq!(result.lines[10].get_count(), 2500);
+        // assert_eq!(result.lines[11].get_count(), 506250);
+    }
+
+    #[test]
+    fn report_sum_works() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+        let result = Report::from_str(input).unwrap();
+        assert_eq!(result.lines.len(), 6);
+        assert_eq!(result.lines[0].iterate_sum(5), 1);
+        assert_eq!(result.lines[1].iterate_sum(5), 16384);
+        assert_eq!(result.lines[2].iterate_sum(5), 1);
+        assert_eq!(result.lines[3].iterate_sum(5), 16);
+        assert_eq!(result.lines[4].iterate_sum(5), 2500);
+        assert_eq!(result.lines[5].iterate_sum(5), 506250);
     }
 }
