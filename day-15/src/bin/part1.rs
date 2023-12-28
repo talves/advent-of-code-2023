89e949db65c257d3cbe0d9cc5fc43a0f7693c4df
Tony Alves <tony@3alves.com>
2023-12-28 10:47:10 -0800

    day-15: part1 tests completed

--- day-15/src/bin/part1.rs
+++ day-15/src/bin/part1.rs
@@ -4,11 +4,31 @@ fn main() {
     dbg!(output);
 }
 
-fn process(input: &str) -> String {
-    format!("Hello, {}", input)
+fn parse_input(input: &str) -> Vec<Vec<u8>> {
+    input
+        .replace("\n", "")
+        .split(",")
+        .map(|x| x.as_bytes().to_vec())
+        .collect()
 }
 
-fn part1(input: &str) -> String {
+fn process(input: &str) -> u64 {
+    let arr = parse_input(input);
+    arr.iter()
+        .map(|sequence| {
+            let mut value = 0;
+            for ascii_val in sequence {
+                value += *ascii_val as u64;
+                value *= 17;
+                value %= 256;
+            }
+            dbg!(&value);
+            value
+        })
+        .sum()
+}
+
+fn part1(input: &str) -> u64 {
     process(input)
 }
 
@@ -17,8 +37,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn hash_works() {
+        let result = part1("HASH");
+        assert_eq!(result, 52);
+    }
+
+    #[test]
     fn part1_works() {
-        let result = part1("Advent of Code!");
-        assert_eq!(result, "Hello, Advent of Code!");
+        let result = part1(
+            "rn=1,cm-,qp=3,cm=2,qp-,pc=4
+,ot=9,ab=5,pc-,pc=6,ot=7",
+        );
+        assert_eq!(result, 1320);
     }
 }
